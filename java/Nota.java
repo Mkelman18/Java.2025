5f4d095210c564ee537d2f5dab59d9db0cff6c93
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-02-18 12:13:57 -0300

    Add files via upload

---
+++ java/Nota.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Nota {
+
+	public static void main (String args[]){
+
+	Scanner sc = new Scanner (System.in);
+
+	System.out.println("Digite a primeira Nota");
+	String nota1 = sc.nextLine();
+
+	
+	
+	}
+}
---
+++ java/Programa.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+public class programa{
+
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        String nomeDaPessoa = sc.nextLine();
+        String numeroDoCartao = sc.nextLine();
+        String cpf = sc.nextLine();
+        
+
+        System.out.println(===============);
+        System.out.println(nomeDaPessoa);
+        System.out.println(numeroDoCartao);
+        System.out.println(cpf);
+        System.out.println(===============);
+
+    }
+}
